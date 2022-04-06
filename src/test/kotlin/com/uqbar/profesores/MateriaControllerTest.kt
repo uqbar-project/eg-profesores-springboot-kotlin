6524f8421b4cd51862a14298eace65e193e335c2
Fernando Dodino <fernando.dodino@gmail.com>
2022-04-05 21:19:15 -0300

    Enhancing test

--- src/test/kotlin/com/uqbar/profesores/MateriaControllerTest.kt
+++ src/test/kotlin/com/uqbar/profesores/MateriaControllerTest.kt
@@ -1,5 +1,6 @@
 package com.uqbar.profesores
 
+import com.uqbar.profesores.domain.Materia
 import com.uqbar.profesores.repos.MateriaRepository
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -10,8 +11,6 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-
-const val MATERIA_SIN_PROFESORES = "Sistemas Operativos"
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,8 +34,19 @@ class MateriaControllerTest {
 
     @Test
     fun `al buscar la informacion de una materia que no existe recibimos como respuesta un codigo de http 404`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/materias/12424"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/materias/124124125125"))
             .andExpect(MockMvcResultMatchers.status().isNotFound)
+    }
+
+    @Test
+    fun `al buscar la informacion de una materia que no tiene docentes recibimos una lista vacia de profesores que la dan`() {
+        val materiaSinProfesores = repoMaterias.save(Materia(nombre = "Geometría Analítica", anio = 1))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/materias/${materiaSinProfesores.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value(materiaSinProfesores.nombre))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(materiaSinProfesores.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.profesores").isEmpty)
     }
 
     @Test
@@ -49,14 +59,5 @@ class MateriaControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.profesores").isArray)
     }
 
-    @Test
-    fun `al buscar la informacion de una materia que no tiene docentes recibimos una lista vacia de profesores que la dan`() {
-        val materia = repoMaterias.findByNombre(MATERIA_SIN_PROFESORES).first()
-        mockMvc.perform(MockMvcRequestBuilders.get("/materias/${materia.id}"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value(materia.nombre))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.anio").value(materia.anio))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.profesores").isEmpty)
-    }
 
 }
