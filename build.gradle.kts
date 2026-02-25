0a58948b892d95e96bf5e6990787626c75fc9e6f
Fernando Dodino <fernando.dodino@gmail.com>
2026-02-25 07:15:06 -0300

    Update 2026 (#2)
    * Update dependencies & gradle
    
    * Set working dependencies and back to JDK 21
    
    * Using JDK 25 finally!
    
    * No, back to JDK 21 and use 2.3.0 Kotlin
    
    * Update docker image for Postgres
    
    * Removing unnecessary dependencies
    
    * Update Springboot dependency
    
    * Align tasks
    
    * Update actions
    
    ---------
    
    Co-authored-by: Fernando Dodino <fernando.dodino@withstensul.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	id("org.springframework.boot") version "3.4.2"
+	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
+	kotlin("plugin.jpa") version "2.3.0"
 	jacoco
 }
 
@@ -36,15 +36,24 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	// testing
-	testImplementation("com.h2database:h2:2.3.232")
+	testImplementation("com.h2database:h2:2.4.240")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    jvmToolchain(21)
+
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xjsr305=strict",
+        )
+    }
+}
+
+tasks.withType<JavaCompile> {
+    targetCompatibility = "21"
+    sourceCompatibility = "21"
 }
 
 tasks.withType<Test> {
@@ -60,7 +69,7 @@ tasks.jacocoTestReport {
 }
 
 jacoco {
-	toolVersion = "0.8.12"
+	toolVersion = "0.8.14"
 }
 
 tasks.jacocoTestReport {
--- src/test/kotlin/com/uqbar/profesores/MateriaControllerWebMvcTest.kt
+++ src/test/kotlin/com/uqbar/profesores/MateriaControllerWebMvcTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
  */
 @WebMvcTest(controllers = [MateriaController::class])
 class MateriaControllerWebMvcTest {
-   @MockBean
+   @MockitoBean
    lateinit var materiaService: MateriaService
 
    @Autowired
